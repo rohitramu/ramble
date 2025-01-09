c531964348bf8209cf438b5c8f490331f240372e
Lin Guo <linsword13@gmail.com>
2025-01-08 23:13:22 -0800

    Cleanup a unused variable

--- var/ramble/repos/builtin/workflow_managers/slurm/workflow_manager.py
+++ var/ramble/repos/builtin/workflow_managers/slurm/workflow_manager.py
@@ -25,14 +25,6 @@ _STATUS_MAP = {
     "CANCELLED+": "CANCELLED",
 }
 
-_ensure_job_id_snippet = r"""
-job_id=$(< {experiment_run_dir}/.slurm_job)
-if [ -z "${job_id:-}" ]; then
-    echo "No valid job_id found" 1>&2
-    exit 1
-fi
-"""
-
 
 class Slurm(WorkflowManagerBase):
     """Slurm workflow manager"""
